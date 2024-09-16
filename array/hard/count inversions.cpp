d6c42f5afc1634ebfbcd7abb9980566c8a23a442
soul059 <keval059@outlook.com>
2024-09-16 23:22:35 +0530

    day end with 2 hard problems

---
+++ array/hard/count inversions.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace  std;
+
+
+// you can declare cnt in globle
+int merge(vector<int> &arr,int low ,int mid ,int high){
+    int left = low;
+    int right = mid+1;
+    vector<int> temp;
+    int cnt=0;
+    while (left <= mid && right <= high)
+    {
+        // right >= left
+        if(arr[left] <= arr[right] ){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        // left > right
+        else{
+            temp.push_back(arr[right]);
+            cnt += mid -left +1;
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i-low];
+    }
+    
+    return cnt;
+    
+}
+
+int mergeSort(vector<int> &arr,int low,int high){
+    int cnt=0;
+    if(low >= high) return cnt;
+    int mid = (low + high)/2;
+    cnt += mergeSort(arr,low,mid);
+    cnt += mergeSort(arr,mid+1,high);
+    cnt += merge(arr,low,mid,high);
+    return cnt;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int res = mergeSort(arr,0,n-1);
+    cout<<"count is: "<<res;
+    return 0;
+}
---
+++ array/hard/count reverse pairs.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace  std;
+
+
+// you can declare cnt in globle
+void merge(vector<int> &arr,int low ,int mid ,int high){
+    int left = low;
+    int right = mid+1;
+    vector<int> temp;
+    int cnt=0;
+    while (left <= mid && right <= high)
+    {
+        // right >= left
+        if(arr[left] <= arr[right] ){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        // left > right
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i-low];
+    }
+    
+    return;
+    
+}
+
+int countPair(vector<int> &arr,int low ,int mid ,int high){
+    int count =0;
+    int right = mid+1;
+    for(int i=low;i<=mid;i++){
+        while(right <=high && arr[i] > 2*arr[right])
+            right++;
+        count += (right - (mid+1));
+    }
+    return count;
+}
+
+int mergeSort(vector<int> &arr,int low,int high){
+    int cnt=0;
+    if(low >= high) return cnt;
+    int mid = (low + high)/2;
+    cnt += mergeSort(arr,low,mid);
+    cnt += mergeSort(arr,mid+1,high);
+    cnt += countPair(arr,low,mid,high);
+    merge(arr,low,mid,high);
+    return cnt;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int res = mergeSort(arr,0,n-1);
+    cout<<"count is: "<<res;
+    return 0;
+}

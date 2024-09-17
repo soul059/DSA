6a7a04d94d3f37f7933f5494e0592486b353d94a
soul059 <keval059@outlook.com>
2024-09-17 09:19:04 +0530

    day start with last array problem in striver a2z DSA

---
+++ array/hard/maxumum product sub array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace  std;
+
+int maxProductSubarr(vector<int> arr,int n){
+    int maxi = INT_MIN;
+    int prefix =1;
+    int suffix = 1;
+    for(int i=0;i<n;i++){
+        if(prefix == 0) prefix = 1;
+        if(suffix == 0) suffix = 1;
+        prefix = prefix * arr[i];
+        suffix = suffix * arr[n-i-1];
+        maxi = max(maxi,max(prefix,suffix));
+    }
+
+    return maxi;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int res = maxProductSubarr(arr,n);
+    cout<<"max product subarray's product is: "<<res<<endl;
+    return 0;
+}

05a32169e41e0ebddcb67d9d3724981b4aee2d1f
soul059 <keval059@outlook.com>
2024-09-19 19:34:45 +0530

    start with 2 binary serch array problem

---
+++ binary search/find firsrt and last occ of x in array.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace  std;
+
+int lowerBound(vector<int> arr,int x){
+    int low =0,high=arr.size()-1;
+    int ans = arr.size();
+    while (low <=high){
+        int mid = (low + high) / 2;
+        if(arr[mid] >= x ){
+            ans = mid;
+            high = mid-1;
+        }
+        else low = mid+1;
+    }
+
+    return ans;
+    
+} 
+
+int upperBound(vector<int> arr,int x){
+    int low =0,high=arr.size()-1;
+    int ans = arr.size();
+    while (low <=high){
+        int mid = (low + high) / 2;
+        if(arr[mid] > x ){
+            ans = mid;
+            high = mid-1;
+        }
+        else low = mid+1;
+    }
+
+    return ans;
+    
+} 
+
+pair<int,int> firstAndLast(vector<int> arr,int x){
+    int lb = lowerBound(arr,x);
+    if(lb == arr.size() && arr[lb] != x) return {-1,-1};
+    return {lb,upperBound(arr,x) -1};
+}
+
+int main(){
+    int n,x;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cin>>x;
+
+    pair<int,int> ans = firstAndLast(arr,x);
+    cout<<"First occ at index: "<<ans.first<<"\t\t Last occ at index: "<<ans.second<<endl;
+    return 0;
+}
---
+++ binary search/find how many times k occer in array.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace  std;
+
+int lowerBound(vector<int> arr,int x){
+    int low =0,high=arr.size()-1;
+    int ans = arr.size();
+    while (low <=high){
+        int mid = (low + high) / 2;
+        if(arr[mid] >= x ){
+            ans = mid;
+            high = mid-1;
+        }
+        else low = mid+1;
+    }
+
+    return ans;
+    
+} 
+
+int upperBound(vector<int> arr,int x){
+    int low =0,high=arr.size()-1;
+    int ans = arr.size();
+    while (low <=high){
+        int mid = (low + high) / 2;
+        if(arr[mid] > x ){
+            ans = mid;
+            high = mid-1;
+        }
+        else low = mid+1;
+    }
+
+    return ans;
+    
+} 
+
+int firstAndLast(vector<int> arr,int x){
+    int lb = lowerBound(arr,x);
+    if(lb == arr.size() && arr[lb] != x) return -1;
+    return (upperBound(arr,x) -1 )-lb+1;
+}
+
+int main(){
+    int n,x;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cin>>x;
+
+    int ans = firstAndLast(arr,x);
+    cout<<"occ of k in array is: "<<ans<<endl;
+    return 0;
+}

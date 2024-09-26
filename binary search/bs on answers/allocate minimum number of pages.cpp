f8830560a136a5969c139f0698babfef753ca6a4
soul059 <keval059@outlook.com>
2024-09-26 23:09:39 +0530

    done 1 problem

---
+++ binary search/bs on answers/allocate minimum number of pages.cpp
@@ -0,0 +1,64 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countStudents(vector<int> &arr, int pages)
+{
+    int n = arr.size(); // size of array.
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pagesStudent + arr[i] <= pages)
+        {
+            // add pages to current student
+            pagesStudent += arr[i];
+        }
+        else
+        {
+            // add pages to next student
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int> &arr, int n, int m)
+{
+    // book allocation impossible:
+    if (m > n)
+        return -1;
+
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+        if (students > m)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+
+    int n,m;
+    cin >> n>>m;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int ans = findPages(arr, n, m);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}

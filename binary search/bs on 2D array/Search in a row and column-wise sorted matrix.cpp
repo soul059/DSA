ab31c6f741e8f06413bd6488403778c2d4532cf9
soul059 <keval059@outlook.com>
2024-10-04 16:41:58 +0530

    1 problem

---
+++ binary search/bs on 2D array/Search in a row and column-wise sorted matrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace  std;
+
+pair<int , int> rowWithMax1s(vector<vector<int>> &matrix,int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int row = 0, col = m - 1;
+
+    //traverse the matrix from (0, m-1):
+    while (row < n && col >= 0) {
+        if (matrix[row][col] == target) return {row,col};
+        else if (matrix[row][col] < target) row++;
+        else col--;
+    }
+    return {-1,-1};
+}
+
+int main(){
+    int n,m,t;
+    cin>>n>>m>>t;
+    vector<vector<int>> mat(n,vector<int>(m));
+    for(int i=0; i<m; i++){
+       for(int j=0;j<n;j++){
+            cin>>mat[i][j];
+       }
+    }
+
+    pair<int,int> ans = rowWithMax1s(mat,t);
+        
+    cout << "index is:"<<ans.first<<","<<ans.second << endl;
+    
+    return 0;
+}

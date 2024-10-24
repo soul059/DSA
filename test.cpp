a1001915bd0b3a68cfa11b544f876eda7803e4f2
soul059 <keval059@outlook.com>
2024-10-24 21:00:11 +0530

    for strick

--- test.cpp
+++ test.cpp
@@ -6,10 +6,6 @@ using namespace  std;
 int main(){
     int n;
     cin>>n;
-    vector<int> arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
-
-    for(auto it: arr) cout<<it<<" ";
-    cout<<endl;
+    cout<<"hello world!"<<"\tn"<<endl;
     return 0;
 }

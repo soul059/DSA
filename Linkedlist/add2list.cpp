7d7292b14ccf46a34bebc2eb1be1219541e22d3f
soul059 <keval059@outlook.com>
2025-07-06 01:04:41 +0530

    2 ll problems

---
+++ Linkedlist/add2list.cpp
@@ -0,0 +1,219 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+template<typename T>
+class Node {
+public:
+    T data;
+    Node* next;
+
+    Node(T val) : data(val), next(nullptr) {}
+};
+
+template<typename T>
+class LinkedList {
+private:
+    Node<T>* head;
+public:
+    LinkedList() : head(nullptr) {}
+
+    Node<T>* getHead() {
+        return head;
+    }
+    void insert(T val) {
+        Node<T>* newNode = new Node<T>(val);
+        if (!head) {
+            head = newNode;
+        } else {
+            Node<T>* temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    void display() {
+        Node<T>* temp = head;
+        while (temp) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl;
+    }
+
+    void creatFromArray(const vector<T>& arr) {
+        for (T val : arr) {
+            insert(val);
+        }
+    }
+    void deleteHead() {
+        if (!head) return;
+        Node<T>* temp = head;
+        head = head->next;
+        delete temp;
+    }
+    void deleteTail() {
+        if (!head) return;
+        if (!head->next) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+        Node<T>* temp = head;
+        while (temp->next && temp->next->next) {
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = nullptr;
+    }
+    void deleteNode(T val) {
+        if (!head) return;
+        if (head->data == val) {
+            deleteHead();
+            return;
+        }
+        Node<T>* temp = head;
+        while (temp->next && temp->next->data != val) {
+            temp = temp->next;
+        }
+        if (temp->next) {
+            Node<T>* toDelete = temp->next;
+            temp->next = toDelete->next;
+            delete toDelete;
+        }
+    }
+    void deleteList() {
+        while (head) {
+            deleteHead();
+        }
+    }
+    void deleteKthNode(int k) {
+        if (k <= 0 || !head) return;
+        if (k == 1) {
+            deleteHead();
+            return;
+        }
+        Node<T>* temp = head;
+        for (int i = 1; i < k - 1 && temp->next; ++i) {
+            temp = temp->next;
+        }
+        if (temp->next) {
+            Node<T>* toDelete = temp->next;
+            temp->next = toDelete->next;
+            delete toDelete;
+        }
+    }
+    void insertAtHead(T val) {
+        Node<T>* newNode = new Node<T>(val);
+        newNode->next = head;
+        head = newNode;
+    }
+    void insertAtTail(T val) {
+        Node<T>* newNode = new Node<T>(val);
+        if (!head) {
+            head = newNode;
+        } else {
+            Node<T>* temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    void insertAtK(T val, int k) {
+        if (k <= 0) return;
+        if (k == 1) {
+            insertAtHead(val);
+            return;
+        }
+        Node<T>* newNode = new Node<T>(val);
+        Node<T>* temp = head;
+        for (int i = 1; i < k - 1 && temp; ++i) {
+            temp = temp->next;
+        }
+        if (temp) {
+            newNode->next = temp->next;
+            temp->next = newNode;
+        } else {
+            delete newNode;
+        }
+    }
+    void insertBeforevalue(T val, T target) {
+        if (!head) return;
+        if (head->data == target) {
+            insertAtHead(val);
+            return;
+        }
+        Node<T>* temp = head;
+        while (temp->next && temp->next->data != target) {
+            temp = temp->next;
+        }
+        if (temp->next) {
+            Node<T>* newNode = new Node<T>(val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+    void insertAfterValue(T val, T target) {
+        Node<T>* temp = head;
+        while (temp) {
+            if (temp->data == target) {
+                Node<T>* newNode = new Node<T>(val);
+                newNode->next = temp->next;
+                temp->next = newNode;
+                return;
+            }
+            temp = temp->next;
+        }
+    }
+    void reverse() {
+        Node<T>* prev = nullptr;
+        Node<T>* current = head;
+        Node<T>* next = nullptr;
+        while (current) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+    ~LinkedList() {
+        deleteList();
+    }
+};
+
+LinkedList<int> add2list(LinkedList<int>& list1, LinkedList<int>& list2) {
+    LinkedList<int> list;
+    Node<int>* head1 = list1.getHead();
+    Node<int>* head2 = list2.getHead();
+    int carry = 0;
+    while (head1 || head2 || carry) {
+        int sum = carry;
+        if (head1) {
+            sum += head1->data;
+            head1 = head1->next;
+        }
+        if (head2) {
+            sum += head2->data;
+            head2 = head2->next;
+        }
+        carry = sum / 10;
+        list.insert(sum % 10);
+    }
+
+    return list;
+}
+
+int main(){
+    LinkedList<int> list1;
+    LinkedList<int> list2;
+    list1.creatFromArray({2, 4, 3}); // Represents the number 342
+    list2.creatFromArray({5, 6, 4}); // Represents the number 465
+    LinkedList<int> result = add2list(list1, list2);
+    result.display(); // Should display the sum as 7 -> 0 -> 8, representing the number 807
+    // (342 + 465 = 807)
+    return 0;
+}
